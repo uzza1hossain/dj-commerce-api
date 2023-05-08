7358d68b9d2cdf74b2b8f0f22bb687ad50c7eb47
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-05-09 00:20:27 +0600

    ✨ (users): Add CustomUser admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+    ]
