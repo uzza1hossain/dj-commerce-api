89a224d25f05a41b97bd6cde4b524a1bd40de833
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-05-14 09:38:59 +0600

    fix

--- address/admin.py
+++ address/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Address
+from .models import Country
+from .models import State
 # Register your models here.
+admin.site.register(Address)
+admin.site.register(Country)
+admin.site.register(State)
