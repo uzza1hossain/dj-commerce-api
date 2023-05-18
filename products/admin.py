59fa8b247ed9198604125d4eb51c4a641fefd65f
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-05-18 23:44:05 +0600

    formatting

--- products/admin.py
+++ products/admin.py
@@ -11,66 +11,66 @@ from .models import VariantAttributeThrough
 
 @admin.register(ProductAttribute)
 class ProductAttributeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
-    search_fields = ('name',)
+    list_display = ("id", "name", "description")
+    search_fields = ("name",)
 
 
 @admin.register(ProductAttributeValue)
 class ProductAttributeValueAdmin(admin.ModelAdmin):
-    list_display = ('id', 'attribute', 'value', 'description')
-    list_filter = ('attribute',)
+    list_display = ("id", "attribute", "value", "description")
+    list_filter = ("attribute",)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
-        'name',
-        'slug',
-        'sku',
-        'description',
-        'owner',
-        'category',
-        'brand',
-        'assets',
-        'is_active',
-        'retail_price',
-        'store_price',
-        'is_digital',
-        'weight',
-        'created_at',
-        'updated_at',
+        "id",
+        "name",
+        "slug",
+        "sku",
+        "description",
+        "owner",
+        "category",
+        "brand",
+        "assets",
+        "is_active",
+        "retail_price",
+        "store_price",
+        "is_digital",
+        "weight",
+        "created_at",
+        "updated_at",
     )
     list_filter = (
-        'owner',
-        'category',
-        'brand',
-        'assets',
-        'is_active',
-        'is_digital',
-        'created_at',
-        'updated_at',
+        "owner",
+        "category",
+        "brand",
+        "assets",
+        "is_active",
+        "is_digital",
+        "created_at",
+        "updated_at",
     )
-    raw_id_fields = ('attributes',)
-    search_fields = ('name', 'slug')
-    prepopulated_fields = {'slug': ['name']}
-    date_hierarchy = 'created_at'
+    raw_id_fields = ("attributes",)
+    search_fields = ("name", "slug")
+    prepopulated_fields = {"slug": ["name"]}
+    date_hierarchy = "created_at"
 
 
 @admin.register(ProductVariant)
 class ProductVariantAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product', 'stock')
-    list_filter = ('product',)
-    raw_id_fields = ('attributes',)
+    list_display = ("id", "product", "stock")
+    list_filter = ("product",)
+    raw_id_fields = ("attributes",)
 
 
 @admin.register(VariantAttributeThrough)
 class VariantAttributeThroughAdmin(admin.ModelAdmin):
-    list_display = ('id', 'variant', 'attribute_value')
-    list_filter = ('variant', 'attribute_value')
+    list_display = ("id", "variant", "attribute_value")
+    list_filter = ("variant", "attribute_value")
 
 
 @admin.register(ProductAttributeThrough)
 class ProductAttributeThroughAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product', 'attribute', 'value')
-    list_filter = ('product', 'attribute', 'value')
+    list_display = ("id", "product", "attribute", "value")
+    list_filter = ("product", "attribute", "value")
